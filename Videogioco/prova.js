b0d21afbb9780381c5d69d346651f87b77c85bdd
Isa <isabel.ciartano@gmail.com>
2026-01-16 12:18:18 +0100

    update

--- Videogioco/prova.js
+++ Videogioco/prova.js
@@ -2,6 +2,8 @@ let backimg;
 let omino;
 let omino2;
 let x;
+let y;
+let ferm;
 
 function preload(){//carica le immagini del videogioco
     backimg=loadImage('./img/sfondo.jpg');
@@ -16,12 +18,34 @@ function setup(){
    // createCanvas(800,600);grandezza schermo videogioco (lunghezza, altezza)
     frameRate(20);//numero fotogrammi al secondo
     x=5;
+    y=550;
+    ferm=0;
 }
 
-function draw(){
-    background(backimg);
-    image(omino,x,400);
-    x=x+10;
+function draw(){//va in loop per il frame rate ridisegna tutto   
+    background(backimg); 
+    image(omino,x,y);
+    if((x+omino.width)>=(backimg.width-1) &&ferm==0 ){
+        ferm=1;
+    }else{ 
+        if(ferm==0){
+            x=x+10;
+        }else{
+            if((x+omino.width)==5 &&ferm==1){
+            ferm=0;
+            }else{
+                if(ferm==1){
+                    x=x-10;     
+                }
+                
+              
+            }
+            
+        }
+       
+    }
+
+    
 }
 
 function mouseClicked(){
